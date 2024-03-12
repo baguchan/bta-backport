fe933727e89e39b12435f41ea8e745f10d08341e
bdmajora <bdmajora@protonmail.com>
2024-03-11 22:53:44 -0600

    added new disk

--- src/main/java/bdmajora/backport/backport.java
+++ src/main/java/bdmajora/backport/backport.java
@@ -1,20 +1,53 @@
 package bdmajora.backport;
 
+import bdmajora.backport.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.helper.BlockBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
-import useless.prismaticlibe.helper.SoundHelper;
+import turniplabs.halplibe.helper.AchievementHelper;
+import turniplabs.halplibe.util.ConfigHandler;
+import turniplabs.halplibe.helper.SoundHelper;
 
+import java.util.Properties;
 
 public class backport implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+
+	static {
+		//this is here to possibly fix some class loading issues, do not delete
+		try {
+			Class.forName("net.minecraft.core.block.Block");
+			Class.forName("net.minecraft.core.item.Item");
+		} catch (ClassNotFoundException ignored) {}
+	}
+
 	public static final String MOD_ID = "BackPort";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	private void handleConfig() {
+		Properties prop = new Properties();
+		prop.setProperty("starting_item_id","30000");
+		ConfigHandler config = new ConfigHandler(MOD_ID,prop);
+		UtilIdRegistrar.initIds(config.getInt("starting_item_id"));
+		config.updateConfig();
+	}
+
+
 	@Override
 	public void onInitialize() {
-		LOGGER.info("BackPort initialized.");
+		LOGGER.info("BackPort loading! watch out for bugs");
+		handleConfig();
+
+		// Register items
+		ModItems.register();
+
+		// Add streaming for the music disc
+		// Assuming you have a method similar to SoundHelper.addStreaming in your code
+		SoundHelper.addStreaming(backport.MOD_ID, "disc_pig_step.ogg");
+
+		LOGGER.info("BackPort loaded successfully!");
 	}
 
 	@Override
