74588f4e9c3d0a9acc97ebe54b81c87959e63189
bdmajora <bdmajora@protonmail.com>
2024-03-13 01:28:41 -0600

    Trying to fix multiplayer

--- src/main/java/bdmajora/backport/backport.java
+++ src/main/java/bdmajora/backport/backport.java
@@ -3,15 +3,11 @@ package bdmajora.backport;
 import bdmajora.backport.block.ModBlocks;
 import bdmajora.backport.item.ModItems;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
-import turniplabs.halplibe.helper.AchievementHelper;
 import turniplabs.halplibe.util.ConfigHandler;
-import turniplabs.halplibe.helper.SoundHelper;
 
 import java.util.Properties;
 
