9d191e840b0fc2d59988dd29cf03168e24182f57
bdmajora <bdmajora@protonmail.com>
2024-03-13 02:02:48 -0600

    Added c13 disc

--- src/main/java/bdmajora/backport/backportmodclient.java
+++ src/main/java/bdmajora/backport/backportmodclient.java
@@ -1,16 +1,7 @@
 package bdmajora.backport;
 
-import bdmajora.backport.block.ModBlocks;
-import bdmajora.backport.item.ModItems;
-import net.fabricmc.api.ModInitializer;
+
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
-import turniplabs.halplibe.util.GameStartEntrypoint;
-import turniplabs.halplibe.util.RecipeEntrypoint;
-import turniplabs.halplibe.helper.AchievementHelper;
-import turniplabs.halplibe.util.ConfigHandler;
 import turniplabs.halplibe.helper.SoundHelper;
 
 public class backportmodclient implements ClientModInitializer {
@@ -18,5 +9,6 @@ public class backportmodclient implements ClientModInitializer {
 	public void onInitializeClient() {
 		// Add streaming for the music disc
 		SoundHelper.Client.addStreaming(backport.MOD_ID, "disc_pig_step.ogg");
+		SoundHelper.Client.addStreaming(backport.MOD_ID, "music_disc_13.ogg");
 	}
 }
--- src/main/java/bdmajora/backport/item/ModItems.java
+++ src/main/java/bdmajora/backport/item/ModItems.java
@@ -9,6 +9,8 @@ import turniplabs.halplibe.helper.ItemHelper;
 public class ModItems {
 	// Items
 	public static final Item recordPigStep = ItemHelper.createItem(backport.MOD_ID, new ItemRecordAccessor(backport.MOD_ID, UtilIdRegistrar.nextIdItem(), "disc_pig_step"), "disc_pig_step.png");
+	public static final Item recordC13 = ItemHelper.createItem(backport.MOD_ID, new ItemRecordAccessor(backport.MOD_ID, UtilIdRegistrar.nextIdItem(), "music_disc_13"), "music_disc_13.png");
+
 	public static void register() {
 	}
 }
