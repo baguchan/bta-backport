194d50e631fa2ecfe67a78d5d50da3d386ac1346
bdmajora <bdmajora@protonmail.com>
2024-03-17 03:41:37 -0600

    Added the new discs and removed a disc that was already in the game.

--- src/main/java/bdmajora/backport/backportmodclient.java
+++ src/main/java/bdmajora/backport/backportmodclient.java
@@ -9,6 +9,9 @@ public class backportmodclient implements ClientModInitializer {
 	public void onInitializeClient() {
 		// Add streaming for the music disc
 		SoundHelper.Client.addStreaming(backport.MOD_ID, "disc_pig_step.ogg");
-		SoundHelper.Client.addStreaming(backport.MOD_ID, "music_disc_13.ogg");
+		SoundHelper.Client.addStreaming(backport.MOD_ID, "music_disc_11.ogg");
+		SoundHelper.Client.addStreaming(backport.MOD_ID, "music_disc_other_side.ogg");
+		SoundHelper.Client.addStreaming(backport.MOD_ID, "music_disc_relic.ogg");
+		SoundHelper.Client.addStreaming(backport.MOD_ID, "music_disc_5.ogg");
 	}
 }
--- src/main/java/bdmajora/backport/item/ModItems.java
+++ src/main/java/bdmajora/backport/item/ModItems.java
@@ -9,7 +9,10 @@ import turniplabs.halplibe.helper.ItemHelper;
 public class ModItems {
 	// Items
 	public static final Item recordPigStep = ItemHelper.createItem(backport.MOD_ID, new ItemRecordAccessor(backport.MOD_ID, UtilIdRegistrar.nextIdItem(), "disc_pig_step"), "disc_pig_step.png");
-	public static final Item recordC13 = ItemHelper.createItem(backport.MOD_ID, new ItemRecordAccessor(backport.MOD_ID, UtilIdRegistrar.nextIdItem(), "music_disc_13"), "music_disc_13.png");
+	public static final Item record11 = ItemHelper.createItem(backport.MOD_ID, new ItemRecordAccessor(backport.MOD_ID, UtilIdRegistrar.nextIdItem(), "music_disc_11"), "music_disc_11.png");
+	public static final Item recordOtherSide = ItemHelper.createItem(backport.MOD_ID, new ItemRecordAccessor(backport.MOD_ID, UtilIdRegistrar.nextIdItem(), "music_disc_other_side"), "music_disc_other_side.png");
+	public static final Item recordRelic = ItemHelper.createItem(backport.MOD_ID, new ItemRecordAccessor(backport.MOD_ID, UtilIdRegistrar.nextIdItem(), "music_disc_relic"), "music_disc_relic.png");
+	public static final Item record5 = ItemHelper.createItem(backport.MOD_ID, new ItemRecordAccessor(backport.MOD_ID, UtilIdRegistrar.nextIdItem(), "music_disc_5"), "music_disc_5.png");
 
 	public static void register() {
 	}
