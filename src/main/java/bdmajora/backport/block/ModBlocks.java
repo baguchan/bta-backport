da105f8c84aa14fb24a863f1c2606ee92b243d4f
bdmajora <bdmajora@protonmail.com>
2024-03-17 20:10:04 -0600

    Added new blocks

--- src/main/java/bdmajora/backport/block/ModBlocks.java
+++ src/main/java/bdmajora/backport/block/ModBlocks.java
@@ -19,8 +19,9 @@ public class ModBlocks {
 		.setBlockSound(BlockSounds.STONE)
 		.setHardness(1.0f)
 		.setResistance(1.0f)
-		.setTopBottomTexture("blackstone_top.png")
+		.setTopTexture("blackstone_top.png")
 		.setSideTextures("blackstone.png")
+		.setBottomTexture("blackstone.png")
 		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
 		.build(new Block("blackStone",UtilIdRegistrar.nextIdBlock(),Material.stone));
 
@@ -63,6 +64,79 @@ public class ModBlocks {
 		.setTextures("polished_blackstone.png")
 		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
 		.build(new Block("polishedBlackstone",UtilIdRegistrar.nextIdBlock(),Material.stone));
+
+	public static final Block mushroomStem = new BlockBuilder(backport.MOD_ID)
+		.setHardness(1.0f)
+		.setResistance(1.0f)
+		.setTextures("mushroom_stem.png")
+		.setTags(BlockTags.MINEABLE_BY_AXE)
+		.build(new Block("mushroomStem",UtilIdRegistrar.nextIdBlock(),Material.wood));
+
+	public static final Block crackedStoneBricks = new BlockBuilder(backport.MOD_ID)
+		.setBlockSound(BlockSounds.STONE)
+		.setHardness(1.0f)
+		.setResistance(1.0f)
+		.setTextures("cracked_stone_bricks.png")
+		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new Block("crackedStoneBricks",UtilIdRegistrar.nextIdBlock(),Material.stone));
+
+	public static final Block chiseledStoneBricks = new BlockBuilder(backport.MOD_ID)
+		.setBlockSound(BlockSounds.STONE)
+		.setHardness(1.0f)
+		.setResistance(1.0f)
+		.setTextures("chiseled_stone_bricks.png")
+		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new Block("chiseledStoneBricks",UtilIdRegistrar.nextIdBlock(),Material.stone));
+
+	public static final Block endStone = new BlockBuilder(backport.MOD_ID)
+		.setBlockSound(BlockSounds.STONE)
+		.setHardness(1.0f)
+		.setResistance(1.0f)
+		.setTextures("end_stone.png")
+		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new Block("endStone",UtilIdRegistrar.nextIdBlock(),Material.stone));
+
+	public static final Block endStoneBricks = new BlockBuilder(backport.MOD_ID)
+		.setBlockSound(BlockSounds.STONE)
+		.setHardness(1.0f)
+		.setResistance(1.0f)
+		.setTextures("end_stone_bricks.png")
+		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new Block("endStoneBricks",UtilIdRegistrar.nextIdBlock(),Material.stone));
+
+	public static final Block mycelium = new BlockBuilder(backport.MOD_ID)
+		.setBlockSound(BlockSounds.GRASS)
+		.setHardness(1.0f)
+		.setResistance(1.0f)
+		.setTopTexture("my_celium_top.png")
+		.setSideTextures("my_celium_side.png")
+		.setBottomTexture("dirt.png")
+		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new Block("mycelium",UtilIdRegistrar.nextIdBlock(),Material.grass));
+
+	public static final Block emeraldOre = new BlockBuilder(backport.MOD_ID)
+		.setBlockSound(BlockSounds.STONE)
+		.setHardness(1.0f)
+		.setResistance(1.0f)
+		.setTextures("emerald_ore.png")
+		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new Block("emeraldOre",UtilIdRegistrar.nextIdBlock(),Material.stone));
+
+	public static final Block deepslateEmeraldOre = new BlockBuilder(backport.MOD_ID)
+		.setBlockSound(BlockSounds.STONE)
+		.setHardness(1.0f)
+		.setResistance(1.0f)
+		.setTextures("deepslate_emerald_ore.png")
+		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new Block("deepslateEmeraldOre",UtilIdRegistrar.nextIdBlock(),Material.stone));
+
+	public static final Block emeraldBlock = new BlockBuilder(backport.MOD_ID)
+		.setBlockSound(BlockSounds.STONE)
+		.setHardness(1.0f)
+		.setResistance(1.0f)
+		.setTextures("emerald_block.png")
+		.setTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new Block("emeraldBlock",UtilIdRegistrar.nextIdBlock(),Material.stone));
 
 	public static void register() {
 	}
